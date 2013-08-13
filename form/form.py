ded60b8d57213286db362d71710294eae6160de7
ochirnyam baasandorj <obaasandorj@custom.(none)>
2013-08-13 11:31:08 -0400

    edit

---
+++ form/form.py
@@ -0,0 +1,18 @@
+def loading_anime():
+
+f= open('anime.dat', 'r')
+raw_text = f.read()
+f.close()
+animes = raw_text.split('\n\n')
+collection = []
+for anime in animes:
+    collection.append(anime.split('\n'))
+    new_collection = []
+    for anime in collection:
+        new = {}
+        new['name'] = anime[0]
+        new['rating'] = anime[1]
+        new['type'] = anime[2]
+        new['genre'] = anime[3:]
+        new_collection.append(new)
+                                         
