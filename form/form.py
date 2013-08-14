f8cec05304fe039b5ad1817aab2355a5a59d9ed4
Live session user <custom@custom.(none)>
2013-08-14 08:57:42 -0400

    sdf

--- form/form.py
+++ form/form.py
@@ -1,21 +1,5 @@
 def loading_anime():
 
-f = open('anime.dat', 'r')
-raw_text = f.read()
-f.close()
-animes = raw_text.split('\n\n')
-collection = []
-for anime in animes:
-    collection.append(anime.split('\n'))
-    new_collection = []
-    for anime in collection:
-        new = {}
-        new['name'] = anime[0]
-        new['rating'] = anime[1]
-        new['type'] = anime[2]
-        new['genre'] = anime[3:]
-        new_collection.append(new)
-                                         
     f = open('data.dat', 'r')
     raw_text = f.read()
     f.close()
