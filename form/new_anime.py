8a59dab57c57e61fbdea1f2c53d1238553336d91
Sarah Mazur <smazur@rackspace.elkner.net>
2013-08-15 14:24:28 +0000

    edit

---
+++ form/new_anime.py
@@ -0,0 +1,62 @@
+from anime import load_anime
+
+class Anime:
+    def __init__(self, raw_anime):
+        """
+          >>> anime = Anime('A\\nTeen\\nSeries\\nAction\\nAdventure\\nFantasy')
+          >>> anime.name
+          'A'
+          >>> anime.rating
+          'Teen'
+          >>> anime.type
+          'Series'
+          >>> anime.genre
+          ['Action', 'Adventure', 'Fantasy']
+        """
+        raw_anime = raw_anime.strip()
+        split_raw = raw_anime.split('\n')
+    
+        self.name = split_raw[0]
+        self.rating = split_raw[1]
+        self.type = split_raw[2]
+        self.genre = split_raw[3:]
+
+    def __str__(self):
+        s = 'Name: {0}\n'.format(self.name)
+        s += '    Rating: {0}\n'.format(self.rating)
+        s += '    Type: {0}\n'.format(self.type)
+        s += '    Genre: '
+        for genre in self.genre[:-1]:
+            s += '{0}, '.format(genre)
+        s += '{0}\n'.format(self.genre[-1])
+
+        return s
+
+
+
+class Collection:
+    def __init__(self, raw_anime_list):
+        """
+          >>> my_anime = Collection(load_anime('test2.dat'))
+          >>> len(my_anime.anime)
+          2
+          >>> new_collection = Collection(load_anime('test3.dat'))
+          >>> len(new_collection.anime)
+          5
+        """
+        self.anime = []
+
+        for raw_anime in raw_anime_list:
+           self.anime.append(Anime(raw_anime)) 
+
+    def __str__(self):
+        s = ''
+        for anime in self.anime:
+            s += str(anime)
+        return s
+
+        
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
